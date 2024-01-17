38c44c0ed078ebd2837d99109b1b02f2147f2549
Adil <skeezygod@gmail.com>
2024-01-17 17:10:43 +0600

    assignment1

--- root.js
+++ root.js
@@ -1,13 +1,61 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const bmiRoutes = require("./routes/bmiRoutes");
+const fs = require("fs");
 const app = express();
 const port = 3000;
-const bodyParser = require("body-parser");
+const bmiHistoryPath = path.join(__dirname, "data", "bmiHistory.json");
+
+function loadBmiHistory() {
+  try {
+    const data = fs.readFileSync(bmiHistoryPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+function saveBmiData(data) {
+  fs.writeFileSync(bmiHistoryPath, JSON.stringify(data));
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-const bmiRoutes = require("./routes/bmiRoutes");
-app.use("/", bmiRoutes);
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.use("/bmi", bmiRoutes);
+
+app.post("/bmi/calculator", (req, res) => {
+  const height = parseFloat(req.body.height);
+  const weight = parseFloat(req.body.weight);
+
+  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+    return res
+      .status(400)
+      .send("Invalid input. Please provide valid height and weight.");
+  }
+
+  const bmi = weight / Math.pow(height, 2);
+
+  const bmiResult = {
+    height: height,
+    weight: weight,
+    bmi: bmi.toFixed(2),
+    date: new Date().toLocaleString(),
+  };
+
+  const bmiHistory = loadBmiHistory();
+
+  bmiHistory.push(bmiResult);
+
+  saveBmiData(bmiHistory);
+
+  res.send(`BMI Result: ${bmi.toFixed(2)}`);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- routes/bmiRoutes.js
+++ routes/bmiRoutes.js
@@ -1,33 +1,21 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../views/bmiCalculator.html");
+const path = require("path");
+const bmiHistoryPath = path.join(__dirname, "..", "data", "bmiHistory.json");
+function loadBmiHistory() {
+  try {
+    const data = fs.readFileSync(bmiHistoryPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+router.get("/calculator", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "bmiCalculator.html"));
 });
 
-router.post("/bmicalculator", (req, res) => {
-  const height = Number(req.body.height) / 100;
-  const weight = Number(req.body.weight);
-
-  const BMIresult = weight / (height * height);
-
-  if (isNaN(BMIresult)) {
-    res.send("Please enter valid values");
-  } else {
-    let message = "";
-
-    if (BMIresult < 18.5) {
-      message = "Underweight";
-    } else if (BMIresult < 24.9) {
-      message = "Normal weight";
-    } else if (BMIresult < 29.9) {
-      message = "Overweight";
-    } else {
-      message = "Obese";
-    }
-
-    res.send(`Your BMI is ${BMIresult.toFixed(2)} and you are ${message}`);
-  }
+router.get("/history", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "history.html"));
 });
 
 module.exports = router;
